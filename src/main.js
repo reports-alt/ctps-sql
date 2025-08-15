b93d130cff46320324d10a66f31bee9eab96d8b4
reports <reports@pdfdocstream.com>
2025-08-15 13:01:33 +0000

    update

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia';
 import vuetify from './plugins/vuetify'
-import { useFirebaseStore } from '/home/user/ctps-sql/src/store/firebase.js'; // Corrected path
 
 const app = createApp(App)
+const pinia = createPinia(); // Pinia instance created
 app.use(router)
 app.use(pinia)
 app.use(vuetify)
